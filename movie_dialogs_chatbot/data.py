09fe06a4cafc89e5f97f1ecfc6f93205a2a296a7
Akash Sonowal <34851159+akashsonowal@users.noreply.github.com>
2023-02-05 13:04:42 +0530

    update data.py

--- movie_dialogs_chatbot/data.py
+++ movie_dialogs_chatbot/data.py
@@ -90,6 +90,94 @@ with open(datafile, 'w', encoding='utf-8') as outfile:
 print('\n Sample lines from file:')
 printLines(datafile)
 
+PAD_token = 0
+SOS_token = 1
+EOS_token = 2
+
+class Voc:
+    def __init__(self, name):
+        self.name = name
+        self.trimmed = False 
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: 'PAD', SOS_token: 'SOS', EOS_token: 'EOS'}
+        self.num_words = 3
+    
+    def addSentence(self, sentence):
+        for word in sentence.split(' '):
+            self.addWord(word)
+    
+    def addWord(self, word):
+        if word not in self.word2index:
+            self.word2index[word] = self.num_words
+            self.word2count[word] = 1
+            self.index2word[self.num_words] = word
+            self.num_words += 1
+        else:
+            self.word2count[word] += 1
+    
+    def trim(self, min_count):
+        if self.trimmed:
+            return 
+        self.trimmed = True
+        keep_words = []
+        for k, v in self.word2count.items():
+            if v >= min_count:
+                keep_words.append(k)
+        print(f'keep words {len(keep_words)} / {len(self.word2index)} = {(len(keep_words)/len(self.word2index)):.4f}')
+        self.word2index = {}
+        self.word2count = {}
+        self.index2word = {PAD_token: ['PAD'], SOS_token: ['SOS'], EOS_token: ['EOS']}
+        self.num_words = 3
+
+        for word in keep_words:
+            self.addWord(word)
+    
+MAX_LENGTH = 10
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != Mn
+    )
+
+def normalizeString(s):
+    s = unicodeToAscii(s.lower().strip())
+    s = re.sub(r'([.!?])', r' \1', s)
+    s = re.sub(r'[^a-zA-Z.!?]', r' ', s)
+    s = re.sub(r'\s+', r' ', s).strip()
+    return s 
+
+def readVocs(datafile, corpus_name):
+    print('Reading lines...')
+    lines = open(datafile, encoding='utf-8').read().strip().split('\n')
+    pairs = [[normalizeString(s) for s in l.split('\t')] for l in lines]
+    voc = Voc(corpus_name)
+    return voc, pairs 
+
+def filterPair(p):
+    return len(p[0].split()) < MAX_LENGTH and len(p[1].split()) < MAX_LENGTH
+
+def loadPrepareData(corpus, corpus_name, datafile, save_dir):
+    print('Start preparing training data...')
+    voc, pairs = readVocs(datafile, corpus_name)
+    print(f'Read {len(pairs)} sentence pairs')
+    pairs = filterPair(pairs)
+    print(f'Trimmed to {len(pairs)} sentence pairs')
+    print(f'Counting words...')
+    for pair in pairs:
+        voc.addSentence(pair[0])
+        voc.addSentence(pair[1])
+    print(f'Counted words {voc.num_words}')
+    return voc, pairs
+
+save_dir = os.path.join('data', 'save')
+voc, pairs = loadPrepareData(corpus, corpus_name, datafile, save_dir)
+print('\n pairs:')
+for pair in pairs[:10]:
+    print(pair)
+
+
+
+
 
 
 
